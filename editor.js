e01aa731afdb0bc9389a2092ab31f0c45592a78b
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-02-14 06:43:43 +0000

    button link done

--- editor.js
+++ editor.js
@@ -1,3 +1,26 @@
+var flag = true;
+function touch(id) {
+    let el = document.getElementById(id);
+    el.style.backgroundColor = flag ? "#38BDF8" : "";
+    flag = !flag;
+}
+
+
 function bold(){
-    
+    touch('bold');
+}
+function italic(){
+    touch('italic');
+}
+function underline(){
+    touch('underline');
+}
+function left(){
+    touch('left');
+}
+function center(){
+    touch('center');
+}
+function right(){
+    touch('right');
 }
