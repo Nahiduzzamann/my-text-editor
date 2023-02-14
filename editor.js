225c577122d4ddbc3fabe5948a780ae68ddf0856
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-02-14 09:22:39 +0000

    ok

--- editor.js
+++ editor.js
@@ -1,26 +1,35 @@
 var flag = true;
 function touch(id) {
     let el = document.getElementById(id);
-    el.style.backgroundColor = flag ? "#38BDF8" : "";
+    el.style.backgroundColor = flag ? "#38BDF8" : "";   
+}
+function change(className){
+    let textEl = document.getElementById('textArea');
+    flag ? textEl.classList.add(className) : textEl.classList.remove(className);
     flag = !flag;
 }
 
-
 function bold(){
     touch('bold');
+    change('font-bold');
 }
 function italic(){
     touch('italic');
+    change('italic');
 }
 function underline(){
     touch('underline');
+    change('underline');
 }
 function left(){
     touch('left');
+    change('text-left');
 }
 function center(){
     touch('center');
+    change('text-center');
 }
 function right(){
     touch('right');
+    change('text-right');
 }
